c281e276801e4dfb846e6ac0ee2413321c02fad8
GerrysatriaH <gerrysatria10@gmail.com>
2023-05-21 10:31:32 +0700

    Add pelanggan and supplier model

---
+++ app/Models/PelangganModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PelangganModel extends Model {
+
+    protected $table = 'pelanggan';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+
+    protected $allowedFields = ['nama, gender, tipe, alamat'];
+}
---
+++ app/Models/SupplierModel.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class SupplierModel extends Model {
+
+    protected $table = 'supplier';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    
+    protected $allowedFields = ['role'];   
+}
