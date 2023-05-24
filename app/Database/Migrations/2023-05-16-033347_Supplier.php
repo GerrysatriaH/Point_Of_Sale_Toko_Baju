086f2231815bb7c14dafab3dc9b1f79dc1a8da8d
GerrysatriaH <gerrysatria10@gmail.com>
2023-05-24 09:31:29 +0700

    Add Supplier Migration

---
+++ app/Database/Migrations/2023-05-16-033347_Supplier.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Supplier extends Migration {
+
+	public function up() {
+
+		// Supplier
+		$this->forge->addField([
+
+			'id'                => [
+                'type'              => 'INT', 
+                'constraint'        => 11, 
+                'unsigned'          => true, 
+                'auto_increment'    => true
+            ],
+			'nama'      => [
+                'type'          => 'VARCHAR', 
+                'constraint'    => 50
+            ],
+			'no_telp'      => [
+                'type'          => 'VARCHAR', 
+                'constraint'    => 20
+            ],
+			'alamat'    => [
+                'type'          => 'VARCHAR', 
+                'constraint'    => 100
+            ],
+			'created_at datetime default current_timestamp',
+			'updated_at datetime default current_timestamp on update current_timestamp'
+		]);
+
+		$this->forge->addKey('id', true);
+		$this->forge->createTable('supplier', true);
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down() {
+
+		$this->forge->dropTable('supplier', true);
+	}
+}
